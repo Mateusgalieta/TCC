fb245ce6582c9891d51eaf3344d145385748643e
Mateus Galieta <mateusgalieta14@gmail.com>
2021-08-25 22:22:37 -0300

    text adjust

--- resources/views/user/register.blade.php
+++ resources/views/user/register.blade.php
@@ -12,7 +12,7 @@
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
-              <li class="breadcrumb-item active">Adicionar Departamento</li>
+              <li class="breadcrumb-item active">Adicionar Usuário</li>
             </ol>
           </div>
         </div>
