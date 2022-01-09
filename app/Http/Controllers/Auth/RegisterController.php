a0c10e26de52e5cf9efee09ef40e0ac3518d42a4
Mateus Galieta <mateusgalieta14@gmail.com>
2022-01-09 18:23:06 -0300

    teste

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -64,14 +64,12 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        User::create([
+        return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
             'organization_id' => $data['organization_id'],
             'status'          => 'AGUARDANDO'
         ]);
-
-        return redirect()->back();
     }
 }
