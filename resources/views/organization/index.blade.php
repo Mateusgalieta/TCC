d4f01ec26b1b8e9437b54149ae1d04b1590a7a1b
Mateus Galieta <mateusgalieta14@gmail.com>
2022-02-11 20:21:08 -0300

    adjust

--- resources/views/organization/index.blade.php
+++ resources/views/organization/index.blade.php
@@ -7,7 +7,7 @@
           <div class="card">
             {!! Form::open(['method' => 'GET']) !!}
             <div class="card-header">
-              <h3 class="card-title">Lista de Departamentos</h3>
+              <h3 class="card-title">Lista de Organizações</h3>
 
               <div class="card-tools">
                 <div class="input-group input-group-sm" style="width: 150px;">
